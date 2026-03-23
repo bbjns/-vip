e5a6ffc72f534fa7a4da4505749a670e9429b66f
bbjns <xvip9188@gmail.com>
2026-03-23 22:35:06 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 import os
 import ccxt
 from telegram.ext import Updater, CommandHandler
-
 TG_TOKEN = os.getenv(jsc6bot)
-CHAT_ID = str(os.getenv(qy2200))
-
+CHAT_ID = str(os.getenv(qy2200)
 API_KEY = os.getenv(bg_7d494ac9f8b27e9b682f41bc3126ff73)
 API_SECRET = os.getenv(77a3bba9fcb9b0dea28bcadf8ae9e5d6500f881b3612e160735365d5fcdf3abb)
 API_PASSWORD = os.getenv(577779188)
-
 exchange = ccxt.bitget({
-    'apiKey': API_KEY,
-    'secret': API_SECRET,
-    'password': API_PASSWORD,
-    'enableRateLimit': True,
-    'options': {'defaultType': 'swap'}
+'apiKey': API_KEY,
+'secret': API_SECRET,
+'password': API_PASSWORD,
+'enableRateLimit': True,
+'options': {'defaultType': 'swap'}
 })
-
 def check_user(update):
-    return str(update.message.chat_id) == CHAT_ID
-
+return str(update.message.chat_id) == CHAT_ID
 def start(update, context):
     if not check_user(update):
         update.message.reply_text("无权限")
